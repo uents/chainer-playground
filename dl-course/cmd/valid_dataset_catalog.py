216875a2cc70990dd60f39ad00deaf398ac7c4cb
Takamasa Ueno <uents@ymail.com>
2017-03-08 01:02:58 +0900

    学習データのカタログ情報の整形ツールを整理

--- dl-course/cmd/valid_dataset_catalog.py
+++ dl-course/cmd/valid_dataset_catalog.py
@@ -9,7 +9,7 @@ import json
 import jsonschema
 
 def parse_arguments():
-    usage = 'make training dataset catalog (sample code)'
+    usage = 'valid dataset catalog'
     parser = argparse.ArgumentParser(usage=usage)
     parser.add_argument('--catalog-file', type=str, dest='catalog_file', required=True)
     return parser.parse_args()
@@ -17,14 +17,14 @@ def parse_arguments():
 if __name__ == '__main__':
     args = parse_arguments()
 
-    # スキーマそのものを検定。不正な場合は例外が発生
-    schema_file = os.path.join('..', 'train_dataset_catalog_schema.json')
+    # スキーマそのものを検定 (不正な場合は例外が発生)
+    schema_file = os.path.join('..', 'schema', 'dataset_catalog_schema.json')
     with open(schema_file, 'r') as fp:
         catalog_schema = json.load(fp)
     jsonschema.Draft4Validator.check_schema(catalog_schema)
 
-    # カタログ情報のスキーマを使った検定。不正な場合は例外が発生
-    with open('train_dataset_catalog_sample.json', 'r') as fp:
+    # カタログ情報のスキーマを使った検定 (不正な場合は例外が発生)
+    with open(args.catalog_file, 'r') as fp:
         catalog = json.load(fp)
     jsonschema.validate(catalog, catalog_schema)
 
