f8d434df80dd3c2d3e02d0792e17a0a1384a8529
Takamasa Ueno <uents@ymail.com>
2017-04-02 06:49:06 +0000

    YOLOの学習コードを整理

--- dl-course/cmd/yolo/lib/yolo.py
+++ dl-course/cmd/yolo/lib/yolo.py
@@ -118,68 +118,49 @@ class YoloDetector(chainer.Chain):
         h = F.leaky_relu(self.fc1(h), slope=0.1)
         h = F.dropout(h, train=self.train, ratio=0.5)
 #        h = F.leaky_relu(self.fc2(h), slope=0.1)
-        h = F.sigmoid(self.fc3(h))
-        # reshape predicted tensors
+        h = self.fc3(h)
+        # normalize and reshape predicted tensors
+        h = F.sigmoid(h)
         h = F.reshape(h, (batch_size, (5*N_BOXES)+N_CLASSES, N_GRID, N_GRID))
         return h
-#        x, y, w, h, conf, prob = F.split_axis(h, indices_or_sections=(1,2,3,4,5), axis=1)
-#        return F.sigmoid(x), F.sigmoid(y), F.sigmoid(w), F.sigmoid(h), F.sigmoid(conf), F.sigmoid(prob)
-
-    '''
-    def __call__(self, x, t):
-        h = self.forward(x)
-        self.h = h.data
-        self.loss = chainer.Variable(xp.asarray([10.]).astype(np.float32))
-        return self.loss
-    '''
 
     def __call__(self, x, t):
         # 推論を実行
         h = self.forward(x)
-        px, py, pw, ph, pconf, pprob = F.split_axis(h, indices_or_sections=(1,2,3,4,5), axis=1)
+        px, py, pw, ph, pconf, pprob \
+            = F.split_axis(h, indices_or_sections=(1,2,3,4,5), axis=1)
         # 教師データを抽出
-        if self.gpu >= 0: t.to_cpu()
-        tx, ty, tw, th, tconf, _tprob = np.array_split(t.data, indices_or_sections=(1,2,3,4,5), axis=1)
-#        tx, ty, tw, th, tconf, tprob = F.split_axis(t, indices_or_sections=(1,2,3,4,5), axis=1)
-        if self.gpu >= 0: t.to_gpu()
+        tx, ty, tw, th, tconf, tprob \
+            = np.array_split(chainer.cuda.to_cpu(t.data), indices_or_sections=(1,2,3,4,5), axis=1)
 
         # オブジェクトが存在しないグリッドは、グリッド中心とする
-        tx[tconf != 1.0] = 0.5
-        ty[tconf != 1.0] = 0.5
-        # オブジェクトが存在しないグリッドは、学習させない
-        if self.gpu >= 0: pprob.to_cpu()
+        tx[tconf != 1.] = 0.5
+        ty[tconf != 1.] = 0.5
+        # オブジェクトが存在しないグリッドは、学習させない(誤差を相殺する)
+        class_map = (tprob == 1.0)
         tprob = pprob.data.copy()
-        if self.gpu >= 0: pprob.to_gpu()
-        tprob[_tprob == 1.0] = 1.0
+        tprob[class_map] = 1.0
         # 学習係数を、オブジェクトが存在するグリッドか否かで調整
-        box_learning_scale = np.tile(0.1, tconf.shape)
-        box_learning_scale[tconf == 1.0] = 5.0
-        conf_learning_scale = np.tile(0.5, tconf.shape)
-        conf_learning_scale[tconf == 1.0] = 1.0
-        prob_learning_scale = np.tile(0.0, tconf.shape)
-        prob_learning_scale[tconf == 1.0] = 1.0
+        box_scale_factor = np.tile(0.1, tconf.shape).astype(np.float32)
+        box_scale_factor[tconf == 1.0] = 5.0
+        conf_scale_factor = np.tile(0.5, tconf.shape).astype(np.float32)
+        conf_scale_factor[tconf == 1.0] = 1.0
+        prob_scale_factor = np.tile(0.0, tconf.shape).astype(np.float32)
+        prob_scale_factor[tconf == 1.0] = 1.0
 
         # 損失誤差を算出
-        tx = self.__variable(tx, np.float32)
-        ty = self.__variable(ty, np.float32)
-        tw = self.__variable(tw, np.float32)
-        th = self.__variable(th, np.float32)
-        tconf = self.__variable(tconf, np.float32)
-        tprob = self.__variable(tprob, np.float32)
-        if self.gpu >=0:
-            tx.to_gpu(), ty.to_gpu(), tw.to_gpu(), th.to_gpu(), tconf.to_gpu(), tprob.to_gpu()
-        box_learning_scale = self.__variable(box_learning_scale, np.float32)
-        conf_learning_scale = self.__variable(conf_learning_scale, np.float32)
-        prob_learning_scale = self.__variable(prob_learning_scale, np.float32)
-        if self.gpu >=0:
-            box_learning_scale.to_gpu(), conf_learning_scale.to_gpu(), prob_learning_scale.to_gpu()
+        tx, ty, tw, th = self.__variable(tx), self.__variable(ty), self.__variable(tw), self.__variable(th)
+        tconf, tprob = self.__variable(tconf), self.__variable(tprob)
+        box_scale_factor = self.__variable(box_scale_factor)
+        conf_scale_factor = self.__variable(conf_scale_factor)
+        prob_scale_factor = self.__variable(prob_scale_factor)
 
-        x_loss = F.sum(box_learning_scale * ((tx - px) ** 2))
-        y_loss = F.sum(box_learning_scale * ((ty - py) ** 2))
-        w_loss = F.sum(box_learning_scale * ((tw - pw) ** 2))
-        h_loss = F.sum(box_learning_scale * ((th - ph) ** 2))
-        conf_loss = F.sum(conf_learning_scale * ((tconf - pconf) ** 2))
-        prob_loss = F.sum(prob_learning_scale * F.reshape(F.sum(((tprob - pprob) ** 2), axis=1), prob_learning_scale.shape))
+        x_loss = F.sum(box_scale_factor * ((tx - px) ** 2))
+        y_loss = F.sum(box_scale_factor * ((ty - py) ** 2))
+        w_loss = F.sum(box_scale_factor * ((tw - pw) ** 2))
+        h_loss = F.sum(box_scale_factor * ((th - ph) ** 2))
+        conf_loss = F.sum(conf_scale_factor * ((tconf - pconf) ** 2))
+        prob_loss = F.sum(prob_scale_factor * F.reshape(F.sum(((tprob - pprob) ** 2), axis=1), prob_scale_factor.shape))
 
         if self.train:
             print("loss x:%03.4f y:%03.4f w:%03.4f h:%03.4f conf:%03.4f prob:%03.4f" %
@@ -192,5 +173,8 @@ class YoloDetector(chainer.Chain):
         else:
             return self.h
 
-    def __variable(self, v, t):
-        return chainer.Variable(xp.asarray(v).astype(t))
+    def __variable(self, v):
+        v = v.astype(np.float32)
+        if self.gpu >= 0:
+            v = chainer.cuda.to_gpu(v)
+        return chainer.Variable(v)
