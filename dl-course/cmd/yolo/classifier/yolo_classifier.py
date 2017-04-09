81feac33801951f8ed35ffb97c63d05247e1d35f
Takamasa Ueno <uents@ymail.com>
2017-04-09 09:20:50 +0900

    YOLOの学習時のデータ選択をミニバッチの度にランダム抽出するよう処理を変更

--- dl-course/cmd/yolo/classifier/yolo_classifier.py
+++ dl-course/cmd/yolo/classifier/yolo_classifier.py
@@ -29,46 +29,37 @@ def load_catalog(catalog_file):
         with open(os.path.join(catalog_file), 'r') as fp:
             catalog = json.load(fp)
     except IOError:
-        return [], []
+        return []
+    return catalog['dataset']
 
-    dataset = catalog['dataset']
+def perform_train(model, optimizer, dataset):
     image_paths = np.asarray([item['color_image_path'] for item in dataset])
+    images = [Image(path, INPUT_SIZE, INPUT_SIZE).image for path in image_paths]
+    # TODO: マルチラベル画像対応 (いらないかも)
     truth_labels = np.asarray([[int(clazz) for clazz in item['classes']] for item in dataset])
-    return image_paths, truth_labels
 
-def one_epoch_train(model, optimizer, image_paths, truth_labels, batch_size):
-    n_train = len(truth_labels)
-    perm = np.random.permutation(n_train)
+    xs = chainer.Variable(xp.asarray(images).astype(np.float32).transpose(0,3,1,2) / 255.)
+    ts = chainer.Variable(xp.asarray(truth_labels.ravel()).astype(np.int32))
 
-    loss, acc = (0., 0.)
-    for count in six.moves.range(0, n_train, batch_size):
-        ix = perm[count:count+batch_size]
-        images = [Image(path, INPUT_SIZE, INPUT_SIZE).image for path in image_paths[ix]]
-        labels = truth_labels[ix]
+    model.train = True
+    optimizer.update(model, xs, ts)
+    return model.loss.data, model.accuracy.data
 
-        xs = chainer.Variable(xp.asarray(images).astype(np.float32).transpose(0,3,1,2) / 255.)
-        # TODO: マルチラベル画像対応 (いらないかも)
-        ts = chainer.Variable(xp.asarray(labels.ravel()).astype(np.int32))
+def perform_cv(model, optimizer, dataset):
+    n_valid = len(dataset)
+    if n_valid == 0: return 0., 0.
 
-        model.train = True
-        optimizer.update(model, xs, ts)
-        print('mini-batch:%d loss:%f acc:%f' % ((count/batch_size)+1, model.loss.data, model.accuracy.data))
-        loss += model.loss.data * len(ix) / n_train
-        acc += model.accuracy.data * len(ix) / n_train
-    return loss, acc
-
-def one_epoch_cv(model, optimizer, image_paths, truth_labels):
-    n_valid = len(truth_labels)
+    image_paths = np.asarray([item['color_image_path'] for item in dataset])
+    # TODO: マルチラベル画像対応 (いらないかも)
+    truth_labels = np.asarray([[int(clazz) for clazz in item['classes']] for item in dataset])
 
     loss, acc = (0., 0.)
     for count in six.moves.range(0, n_valid, 10):
         ix = np.arange(count, min(count+10, n_valid))
         images = [Image(path, INPUT_SIZE, INPUT_SIZE).image for path in image_paths[ix]]
-        labels = truth_labels[ix]
 
         xs = chainer.Variable(xp.asarray(images).astype(np.float32).transpose(0,3,1,2) / 255.)
-        # TODO: マルチラベル画像対応 (いらないかも)
-        ts = chainer.Variable(xp.asarray(labels.ravel()).astype(np.int32))
+        ts = chainer.Variable(xp.asarray(truth_labels[ix].ravel()).astype(np.int32))
 
         model.train = False
         model(xs, ts)
@@ -77,49 +68,48 @@ def one_epoch_cv(model, optimizer, image_paths, truth_labels):
     return loss, acc
 
 def train_model(args):
-    print('train: gpu:%d epoch:%d batch_size:%d' %
-        (args.gpu, args.n_epoch, args.batch_size))
+    print('train: gpu:%d iteration:%d batch_size:%d' %
+        (args.gpu, args.iteration, args.batch_size))
 
     model = YoloClassifier(args.gpu)
     if len(args.model_file) > 0:
         print('load model: %s' % args.model_file)
         chainer.serializers.load_npz(args.model_file, model)
 
-    # TODO: 本学習に合わせてMementum SGDとするか検討
+    # TODO: 本学習に合わせてMomentum SGDに変更する？
     optimizer = chainer.optimizers.Adam()
     optimizer.setup(model)
-    if len(args.state_file) > 0:
-        print('load state: %s' % args.state_file)
-        chainer.serializers.load_npz(args.state_file, optimizer)
+    if len(args.optimizer_file) > 0:
+        print('load optimizer: %s' % args.optimizer_file)
+        chainer.serializers.load_npz(args.optimizer_file, optimizer)
 
-    train_image_paths, train_labels = load_catalog(args.train_catalog_file)
-    cv_image_paths, cv_labels = load_catalog(args.cv_catalog_file)
-    print('number of dataset: train:%d cv:%d' % (len(train_labels), len(cv_labels)))
+    train_dataset = load_catalog(args.train_catalog_file)
+    cv_dataset = load_catalog(args.cv_catalog_file)
+    print('number of dataset: train:%d cv:%d' % (len(train_dataset), len(cv_dataset)))
 
     logs = []
-    for epoch in range(1, args.n_epoch+1):
-        train_loss, train_acc = one_epoch_train(
-            model, optimizer, train_image_paths, train_labels, args.batch_size)
-        cv_loss, cv_acc = one_epoch_cv(
-            model, optimizer, cv_image_paths, cv_labels)
+    for iter_count in six.moves.range(1, args.iteration+1):
+        batch_dataset = np.random.choice(train_dataset, args.batch_size)
+        train_loss, train_acc = perform_train(model, optimizer, batch_dataset)
+        print('mini-batch:%d loss:%f acc:%f' % (iter_count, train_loss, train_acc))
 
-        print('epoch:%d trian loss:%f train acc:%f cv loss:%f cv acc:%f' %
-            (epoch, train_loss, train_acc, cv_loss, cv_acc))
-        logs.append({
-            'epoch': str(epoch),
-            'train_loss': str(train_loss), 'train_acc': str(train_acc),
-            'cv_loss': str(cv_loss), 'cv_acc': str(cv_acc)
-        })
-#        if (epoch % 10) == 0:
-#            chainer.serializers.save_npz('classifier_epoch{}.model'.format(epoch), model)
-#            chainer.serializers.save_npz('classifier_epoch{}.state'.format(epoch), optimizer)
+        if (iter_count == 1) or (iter_count == args.iteration) or (iter_count % 100 == 0):
+            cv_loss, cv_acc = perform_cv(model, optimizer, cv_dataset)
+            print('iter:%d trian loss:%f acc:%f cv loss:%f acc:%f' %
+                (iter_count, train_loss, train_acc, cv_loss, cv_acc))
+            logs.append({
+                'iteration': str(iter_count),
+                'train_loss': str(train_loss), 'train_acc': str(train_acc),
+                'cv_loss': str(cv_loss), 'cv_acc': str(cv_acc)
+            })
+#            chainer.serializers.save_npz('classifier_iter{}.model'.format(str(iter_count).zfill(5)), model)
+#            chainer.serializers.save_npz('classifier_iter{}.state'.format(str(iter_count).zfill(5)), optimizer)
 
-    if len(train_labels) > 0:
-        df_logs = pd.DataFrame(
-            logs, columns=['epoch', 'train_loss', 'train_acc', 'cv_loss', 'cv_acc'])
+    if len(train_dataset) > 0:
+        df_logs = pd.DataFrame(logs,
+            columns=['iteration', 'train_loss', 'train_acc', 'cv_loss', 'cv_acc'])
         with open('train_log.csv', 'w') as fp:
             df_logs.to_csv(fp, encoding='cp932', index=False)
-
         chainer.serializers.save_npz('classifier_final.model', model)
         chainer.serializers.save_npz('classifier_final.state', optimizer)
 
@@ -127,11 +117,11 @@ def parse_arguments():
     description = 'YOLO Classifier'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('--model-file', type=str, dest='model_file', default='')
-    parser.add_argument('--state-file', type=str, dest='state_file', default='')
+    parser.add_argument('--optimizer-file', type=str, dest='optimizer_file', default='')
     parser.add_argument('--train-catalog-file', type=str, dest='train_catalog_file', default='')
     parser.add_argument('--cv-catalog-file', type=str, dest='cv_catalog_file', default='')
     parser.add_argument('--gpu', '-g', type=int, default=-1)
-    parser.add_argument('--epoch', '-e', type=int, dest='n_epoch', default=1)
+    parser.add_argument('--iteration', '-i', type=int, dest='iteration', default=1)
     parser.add_argument('--batchsize', '-b', type=int, dest='batch_size', default=20)
     return parser.parse_args()
 
--- dl-course/cmd/yolo/detector/yolo_trainer.py
+++ dl-course/cmd/yolo/detector/yolo_trainer.py
@@ -38,22 +38,22 @@ weight_decay = 0.005
 
 
 def load_catalog(catalog_file):
-    def dict_to_box(box):
-        return Box(x=float(box['x']), y=float(box['y']),
-                width=float(box['width']), height=float(box['height']),
-                confidence=1., clazz=int(box['class']), objectness=1.)
-
     try:
         with open(os.path.join(catalog_file), 'r') as fp:
             catalog = json.load(fp)
     except IOError:
-        return [], []
-
+        return []
     dataset = filter(lambda item: item['bounding_boxes'] != [], catalog['dataset'])
-    image_paths = np.asarray([item['color_image_path'] for item in dataset])
-    truth_boxes = np.asarray([[dict_to_box(box) for box in item['bounding_boxes']]
-                                for item in dataset])
-    return image_paths, truth_boxes
+    return dataset
+#    image_paths = np.asarray([item['color_image_path'] for item in dataset])
+#    truth_boxes = np.asarray([[dict_to_box(box) for box in item['bounding_boxes']]
+#                                for item in dataset])
+#    return image_paths, truth_boxes
+
+def dict_to_box(box):
+    return Box(x=float(box['x']), y=float(box['y']),
+            width=float(box['width']), height=float(box['height']),
+            confidence=1., clazz=int(box['class']), objectness=1.)
 
 def load_dataset(image_paths, truth_boxes):
     def load(path, boxes):
@@ -104,52 +104,32 @@ def mean_average_precision(positives):
     aps = average_precisions(positives)
     return np.asarray(aps).mean()
 
-def one_epoch_train(model, optimizer, image_paths, ground_truth_boxes, batch_size, epoch):
-    n_train = len(ground_truth_boxes)
-    perm = np.random.permutation(n_train)
-    if n_train == 0:
-        return 0., 0.
+def perform_train(model, optimizer, dataset):
+    image_paths = np.asarray([item['color_image_path'] for item in dataset])
+    real_truth_boxes = np.asarray([[dict_to_box(box) for box in item['bounding_boxes']]
+                                    for item in dataset])
+    images, truth_boxes = load_dataset(image_paths, real_truth_boxes)
+    truth_tensors = np.asarray([boxes_to_tensor(boxes) for boxes in truth_boxes])
 
-    loss = 0.
-    positives = init_positives()
-    for count in six.moves.range(0, n_train, batch_size):
-#        ix = np.arange(count, min(count+batch_size, n_train))
-        ix = perm[count:count+batch_size]
-        images, truth_boxes = load_dataset(image_paths[ix], ground_truth_boxes[ix])
-        truth_tensors = np.asarray([boxes_to_tensor(boxes) for boxes in truth_boxes])
+    xs = chainer.Variable(xp.asarray(images).transpose(0,3,1,2).astype(np.float32) / 255.)
+    ts = chainer.Variable(xp.asarray(truth_tensors).astype(np.float32))
 
-        xs = chainer.Variable(xp.asarray(images).transpose(0,3,1,2).astype(np.float32) / 255.)
-        ts = chainer.Variable(xp.asarray(truth_tensors).astype(np.float32))
+    model.train = True
+    optimizer.update(model, xs, ts)
+    return model.loss.data
 
-        # TODO: Epoch単位のスケジュールに変更
-        iteration = (epoch - 1) * batch_size + count
-        if str(iteration) in learning_schedules:
-            optimizer.lr = learning_schedules[str(iteration)]
+def perform_cv(model, optimizer, dataset):
+    n_valid = len(dataset)
+    if n_valid == 0: return 0., 0.
 
-        model.train = True
-        optimizer.update(model, xs, ts)
-        loss += model.loss.data * len(ix) / n_train
-
-        for batch in six.moves.range(0, len(ix)):
-            predicted_boxes = tensor_to_boxes(model.h[batch])
-            positives = add_positives(
-                positives, count_positives(predicted_boxes, truth_boxes[batch]))
-#            for pred_box, truth_box in itertools.product(predicted_boxes, truth_boxes[batch]):
-#                print('{} {} pred:{} truth:{}'.format(
-#                    count + batch + 1, Box.correct(pred_box, truth_box), pred_box, truth_box))
-
-    return loss, mean_average_precision(positives)
-
-def one_epoch_cv(model, optimizer, image_paths, ground_truth_boxes):
-    n_valid = len(ground_truth_boxes)
-    if n_valid == 0:
-        return 0., 0.
-
+    image_paths = np.asarray([item['color_image_path'] for item in dataset])
+    real_truth_boxes = np.asarray([[dict_to_box(box) for box in item['bounding_boxes']]
+                                    for item in dataset])
     loss = 0.
     positives = init_positives()
     for count in six.moves.range(0, n_valid, 10):
         ix = np.arange(count, min(count+10, n_valid))
-        images, truth_boxes = load_dataset(image_paths[ix], ground_truth_boxes[ix])
+        images, truth_boxes = load_dataset(image_paths[ix], real_truth_boxes[ix])
         truth_tensors = np.asarray([boxes_to_tensor(boxes) for boxes in truth_boxes])
 
         xs = chainer.Variable(xp.asarray(images).transpose(0,3,1,2).astype(np.float32) / 255.)
@@ -170,8 +150,8 @@ def one_epoch_cv(model, optimizer, image_paths, ground_truth_boxes):
     return loss, mean_average_precision(positives)
 
 def train_model(args):
-    print('train model: gpu:%d epoch:%d batch_size:%d' % \
-        (args.gpu, args.n_epoch, args.batch_size))
+    print('train model: gpu:%d iteration:%d batch_size:%d' % \
+        (args.gpu, args.iteration, args.batch_size))
 
     model = YoloDetector(args.gpu)
     if len(args.model_file) > 0:
@@ -181,53 +161,54 @@ def train_model(args):
     optimizer = chainer.optimizers.MomentumSGD(
         lr=learning_schedules['0'], momentum=momentum)
     optimizer.setup(model)
-    if len(args.state_file) > 0:
-        print('load state: %s' % (args.state_file))
-        chainer.serializers.load_npz(args.state_file, optimizer)
+    if len(args.optimizer_file) > 0:
+        print('load optimizer: %s' % (args.optimizer_file))
+        chainer.serializers.load_npz(args.optimizer_file, optimizer)
     optimizer.add_hook(chainer.optimizer.WeightDecay(weight_decay))
     optimizer.use_cleargrads() # TODO: 必要？
 
-    train_image_paths, train_boxes = load_catalog(args.train_catalog_file)
-    cv_image_paths, cv_boxes = load_catalog(args.cv_catalog_file)
-    print('number of dataset: train:%d cv:%d' % (len(train_boxes), len(cv_boxes)))
+    train_dataset = load_catalog(args.train_catalog_file)
+    cv_dataset = load_catalog(args.cv_catalog_file)
+    print('number of dataset: train:%d cv:%d' % (len(train_dataset), len(cv_dataset)))
 
     logs = []
-    for epoch in six.moves.range(1, args.n_epoch+1):
-        train_loss, train_map = one_epoch_train(
-            model, optimizer, train_image_paths, train_boxes, args.batch_size, epoch)
-        cv_loss, cv_map = one_epoch_cv(
-            model, optimizer, cv_image_paths, cv_boxes)
+    for iter_count in six.moves.range(1, args.iteration+1):
+        if str(iter_count) in learning_schedules:
+            optimizer.lr = learning_schedules[str(iter_count)]
 
-        print('epoch:%d trian loss:%f train map:%f cv loss:%f cv map:%f' %
-            (epoch, train_loss, train_map, cv_loss, cv_map))
-        logs.append({
-            'epoch': str(epoch),
-            'train_loss': str(train_loss), 'train_map': str(train_map),
-            'cv_loss': str(cv_loss), 'cv_map': str(cv_map)
-        })
-        if (epoch % 10) == 0:
-            chainer.serializers.save_npz('detector_epoch{}.model'.format(epoch), model)
-            chainer.serializers.save_npz('detector_epoch{}.state'.format(epoch), optimizer)
+        batch_dataset = np.random.choice(train_dataset, args.batch_size)
+        train_loss = perform_train(model, optimizer, batch_dataset)
+#        print('mini-batch:%d loss:%f' % (iter_count, train_loss))
 
-    if len(train_boxes) > 0:
-        df_logs = pd.DataFrame(
-            logs, columns=['epoch', 'train_loss', 'train_map', 'cv_loss', 'cv_map'])
+        if (iter_count == 1) or (iter_count == args.iteration) or (iter_count % 100 == 0):
+            cv_loss, cv_map = perform_cv(model, optimizer, cv_dataset)
+            print('iter:%d trian loss:%f cv loss:%f map:%f' %
+                (iter_count, train_loss, cv_loss, cv_map))
+            logs.append({
+                'iteration': str(iter_count),
+                'train_loss': str(train_loss), 'train_map': str(0.0),
+                'cv_loss': str(cv_loss), 'cv_map': str(cv_map)
+            })
+#            chainer.serializers.save_npz('detector_iter{}.model'.format(str(iter_count).zfill(5)), model)
+#            chainer.serializers.save_npz('detectory_iter{}.state'.format(str(iter_count).zfill(5)), optimizer)
+
+    if len(train_dataset) > 0:
+        df_logs = pd.DataFrame(logs,
+            columns=['iteration', 'train_loss', 'train_map', 'cv_loss', 'cv_map'])
         with open('train_log.csv', 'w') as fp:
             df_logs.to_csv(fp, encoding='cp932', index=False)
-
-        chainer.serializers.save_npz('detector_final.model', model)
-        chainer.serializers.save_npz('detector_final.state', optimizer)
-
+        chainer.serializers.save_npz('detectory_final.model', model)
+        chainer.serializers.save_npz('detectory_final.state', optimizer)
 
 def parse_arguments():
     description = 'YOLO Detection Trainer'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('--model-file', type=str, dest='model_file', default=DETECTOR_FIRST_MODEL_PATH)
-    parser.add_argument('--state-file', type=str, dest='state_file', default='')
+    parser.add_argument('--optimizer-file', type=str, dest='optimizer_file', default='')
     parser.add_argument('--train-catalog-file', type=str, dest='train_catalog_file', default='')
     parser.add_argument('--cv-catalog-file', type=str, dest='cv_catalog_file', default='')
     parser.add_argument('--gpu', '-g', type=int, default=-1)
-    parser.add_argument('--epoch', '-e', type=int, dest='n_epoch', default=1)
+    parser.add_argument('--iteration', '-i', type=int, dest='iteration', default=1)
     parser.add_argument('--batchsize', '-b', type=int, dest='batch_size', default=20)
     return parser.parse_args()
 
